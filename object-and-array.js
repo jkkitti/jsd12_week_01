b6642825462a382ab17e81ab79bfc3a483471f96
jkkitti <j.kittiwiriya@gmail.com>
2026-03-12 21:30:09 +0700

    added 4 new files

---
+++ object-and-array.js
@@ -0,0 +1,11 @@
+const learner = {
+    name:"Aod", 
+    age:30, 
+    isGraduated:false, 
+    hobbies:["traveling", "coding", "reading"]
+};
+
+console.log(learner.name);
+console.log(learner.age);
+console.log(learner.isGraduated);
+console.log(learner.hobbies[0]);
